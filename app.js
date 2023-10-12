fd7e7188627460d5b063bd3b63939b371ec0ecd3
Dwivedi Priya <pd7668665@gmail.com>
2023-10-12 16:17:47 +0530

    New Feature

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 let inp=document.querySelector("input");
 let btn1=document.querySelector(".add");
+let ul=document.querySelector("ul");
+let editbtn=document.querySelector(".edit");
+let delbtn=document.querySelector(".delete");
+let li=document.querySelector("li");
+
 
 btn1.addEventListener("click",function(){
-    console.log("button was clicked");
+    console.log(inp.value);
+    let itemlist=document.createElement("li");
+    itemlist.innerText=inp.value;
+    itemlist.classList.add("item");
+    ul.appendChild(itemlist);
+    inp.value="";
+
+    let editbutton=document.createElement("button");
+    editbutton.classList.add("edit");
+    editbutton.innerText="Edit";
+    itemlist.appendChild(editbutton);
+    
+    let delbutton=document.createElement("button");
+    delbutton.classList.add("delete");
+    delbutton.innerText="delete";
+    itemlist.appendChild(delbutton);
 });
